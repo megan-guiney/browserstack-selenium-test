1aa5f9d43e0b87883937a3c916fcc3ba85e008fd
shy-pangolin <5hy.p4ng0l1n@gmail.com>
2025-02-09 13:19:27 -0800

    initial commit on technical project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# -browserstack-selenium-test
-Bbasic Browserstack-integrated selenium-python test suite, to be run from jenkins
+# Browserstack-selenium-test
+Basic Browserstack-integrated selenium-python test suite, to be run from jenkins
+
+## Challenge Requirements
+
+- [X] Executes on BrowserStack (you will need to create a free trial)
+- The suite must contain a test doing the following:
+  - [X] Log into www.bstackdemo.com using these dummy credentials: demouser/testingisfun99
+  - [X] Filter the product view to show "Samsung" devices only
+  - [X] Favorite the "Galaxy S20+" device by clicking the yellow heart icon
+  - [X] Verify that the Galaxy S20+ is listed on the Favorites page
+- Run across the following three browsers:
+  - [x] Windows 10 Chrome
+  - [] macOS Ventura Firefox
+  - [] Samsung Galaxy S22
+  - [ ] Run in parallel across the 3
+- [ ] Be Executed from Jenkins server
+- [X] Be uploaded as GitHub Repo
+- [ ] Include proof of Jenkins pipeline (screenshots or pipeline code)
+
+# Setup Notes:
+- Must be set up in a python 3.8 venv
---
+++ test_browserstack_demo.py
@@ -0,0 +1,113 @@
+import unittest
+import time
+import os
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from browserstack.local import Local  # Required for BrowserStack Local
+
+
+class TestBStackLogin(unittest.TestCase):
+
+    # @classmethod
+    # def setUpClass(cls):
+    #    print("")
+
+    def setUp(self):
+        print("Ensuring base class is setUp properly", flush=True)
+        super().setUp()
+        self.baseURL = "https://www.bstackdemo.com/"
+        driverUser = os.environ.get("BROWSERSTACK_USERNAME")
+        driverPasswd = os.environ.get("BROWSERSTACK_ACCESS_KEY")
+        if not driverUser or not driverPasswd:
+            raise ValueError(
+                "BrowserStack credentials not found in environment variables!")
+        # TODO: clean up browser string with embedded driver creds
+        browserstackURL = f"https://{driverUser}:{driverPasswd}@hub-cloud.browserstack.com/wd/hub"
+        self.driver = webdriver.Remote(
+            command_executor=browserstackURL,
+            # Options are dynamically set from browserstack.yml
+            options=webdriver.ChromeOptions()
+        )
+
+    def test_favorite(self):
+        # set variables from env
+        # TODO: clean up to better methodology
+        TEST_USR = os.environ.get("BSTACK_USER")
+        TEST_PASSWD = os.environ.get("BSTACK_PASS")
+        driver = self.driver
+
+        # Login to site
+        print("Logging in...")
+        driver.get(self.baseURL + "signin")
+        driver.maximize_window()
+        try:
+            # Wait for the username dropdown container to be visible and click it
+            username_dropdown = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.ID, "username"))
+            )
+            username_dropdown.click()  # Open dropdown
+            # Now locate the actual input inside the dropdown and send the username
+            username_input = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "react-select-2-input"))
+            )
+            username_input.send_keys(TEST_USR)  # Type username
+            username_input.send_keys(Keys.RETURN)  # Select it
+            # Repeat the same steps for password selection
+            password_dropdown = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.ID, "password"))
+            )
+            password_dropdown.click()  # Open password dropdown
+            password_input = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "react-select-3-input"))
+            )
+            password_input.send_keys(TEST_PASSWD)  # Type password
+            password_input.send_keys(Keys.RETURN)  # Select it
+            # Click the login button
+            login_button = driver.find_element(By.ID, "login-btn")
+            login_button.click()
+            print("--> Login successful!")
+
+            print("Filtering Samsung devices...")
+            WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//span[text()='Apple']//ancestor::label"))
+            )
+            samsung_filter = driver.find_element(
+                By.XPATH, "//span[text()='Samsung']//ancestor::label")
+            samsung_filter.click()
+            print("--> Samsung devices filtered!")
+
+            print("Favoriting the Galaxy S20+...")
+            favorite_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//p[text()='Galaxy S20+']/ancestor::div[contains(@class, 'shelf-item')]//button"))
+            )
+            favorite_button.click()
+            print("--> Favorite button pressed!")
+
+            print("Verifying that the Galazy S20+ was successfully added to Favorites...")
+
+            driver.find_element(By.ID, "favourites").click()
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//p[text()='Galaxy S20+']"))
+            )
+            assert driver.find_element(
+                By.XPATH, "//p[text()='Galaxy S20+']").is_displayed(), "Galaxy S20+ is not in Favorites!"
+            print("--> Test Passed: Galaxy S20+ can be added to Favorites")
+
+        except Exception as e:
+            print("Error: " + str(e))
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
